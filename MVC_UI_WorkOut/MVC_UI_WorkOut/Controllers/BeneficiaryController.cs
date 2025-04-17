06615c29f1ff0df06829d6272fe3e63014355642
saranrajk87 <saranrajk87@outlook.com>
2025-04-17 19:18:52 +0530

    MVC_UI_WorkOut
    Initial Setup

---
+++ MVC_UI_WorkOut/MVC_UI_WorkOut/Controllers/BeneficiaryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_UI_WorkOut.Controllers
+{
+    public class BeneficiaryController : Controller
+    {
+        public ActionResult Beneficiaries()
+        {
+            return View();
+        }
+
+        public ActionResult MaritalStatus()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_UI_WorkOut/MVC_UI_WorkOut/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_UI_WorkOut.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult ContentHost()
+        {
+            return View();
+        }
+
+    }
+}
